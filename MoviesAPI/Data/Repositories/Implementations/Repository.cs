7634c217e5d9c16db055b7b6fe473379306f30a7
Arturo Salinas <prico.team@gmail.com>
2022-03-12 15:58:49 -0600

    Adding generic repository to support CRUD for any entity

---
+++ MoviesAPI/Data/Repositories/Implementations/Repository.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using MoviesAPI.Data.Repositories.Interfaces;
+
+namespace MoviesAPI.Data.Repositories.Implementations
+{
+    public class Repository<T> : IRepository<T> where T:class
+    {
+        public MoviesContext _context;
+        public DbSet<T> entity;
+        public Repository(MoviesContext context)
+        {
+            _context=context;
+            entity = _context.Set<T>();
+        }
+
+        public T Get(int id)
+        {
+            return entity.Find(id);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return entity.ToList();
+        }
+
+        public void Insert(T obj)
+        {
+            entity.Add(obj);
+            _context.SaveChanges();
+        }
+
+        public void Update(T obj)
+        {
+            entity.Update(obj);
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var exitingObj = Get(id);
+            if (exitingObj != null)
+            {
+                entity.Remove(exitingObj);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ MoviesAPI/Data/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace MoviesAPI.Data.Repositories.Interfaces
+{
+    public interface IRepository<T> where T:class
+    {
+        T Get(int id);
+        IEnumerable<T> GetAll();
+        void Insert(T obj);
+        void Update(T obj);
+        void Delete(int id);
+    }
+}
